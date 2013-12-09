4bc26d2944372aef9ab72210d6a9441e2583662f
Miikka Virtanen <dragory.up@gmail.com>
2013-12-09 16:48:30 +0200

    Separate grunt tasks to scripts and styles. Add compass task. Add watch target for styles. Ignore and remove app.css from the repository.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-contrib-coffee");
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-compass");
     grunt.loadNpmTasks("grunt-node-webkit-builder");
 
     var gruntConfig = {
@@ -28,7 +29,11 @@ module.exports = function (grunt) {
         watch: {
             scripts: {
                 files: ['app/coffee/**/*.coffee'],
-                tasks: ['default']
+                tasks: ['scripts']
+            },
+            styles: {
+                files: ['app/scss/**/*.scss'],
+                tasks: ['styles']
             }
         },
 
@@ -42,10 +47,21 @@ module.exports = function (grunt) {
                 linux64: true
             },
             src: ['./**/*']
+        },
+
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'app/scss',
+                    cssDir: 'app/css',
+                    environment: 'production'
+                }
+            }
         }
     };
 
     grunt.initConfig(gruntConfig);
 
-    grunt.registerTask("default", ["coffee", "concat"]);
+    grunt.registerTask("scripts", ["coffee", "concat"]);
+    grunt.registerTask("styles", ["compass"]);
 };
