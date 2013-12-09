a3501f2bc0603211ec2081266d03ff3395f918ca
Miikka Virtanen <dragory.up@gmail.com>
2013-12-09 16:39:32 +0200

    Ignore compiled JS files from git. Remove the previously ignored-but-not-removed config.json.

--- app/js/compiled/sortable.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  angular.module('starloader').directive('sortable', [
-    function() {
-      var link;
-      return link = function(scope, element) {};
-    }
-  ]);
-
-}).call(this);
